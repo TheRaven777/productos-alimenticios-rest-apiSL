3c813d9e254773ed839e70d3b8b65e19786da6fe
Lorenzo <lorenzo.1701611861@ucaldas.edu.co>
2022-02-19 11:37:00 -0500

    carpetas de mvc agregadas

---
+++ model/db-connection/mongodb.js
@@ -0,0 +1,6 @@
+// user: productos-alimenticios-rest-api-user
+// password: tjX2wBFP4k7ajr50
+
+
+
+// connectionString mongodb+srv://productos-alimenticios-rest-api-user:<password>@cluster0.hbbwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
