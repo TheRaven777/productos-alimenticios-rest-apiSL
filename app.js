351ef245b3322e01bc8ad731f74b4461e281e8a0
Lorenzo <lorenzo.1701611861@ucaldas.edu.co>
2022-02-20 18:24:43 -0500

    cambios en app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// archivo principal de la api rest
+const express=require("express");
+const config=require("config");
+
+const app=express();
+const port=config.get("server-port");
+
+app.get("/", (req, res, next) => {
+    res.send("Welcome to productos alimenticios rest api.");
+});
+
+app.listen(port, ()=>{
+    console.log("Server is running...")
+});
