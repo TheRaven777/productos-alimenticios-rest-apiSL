372176b032626da7b3a6cee0682fd3bb3ec879b0
Lorenzo <lorenzo.1701611861@ucaldas.edu.co>
2022-02-20 01:45:28 -0500

    creacion de user logic y general helpers

---
+++ model/dto/producto.dto.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const schema = require("../schemas/producto.schema");
+
+schema.statics = {
+    create: function (data, cb){
+        let doc = new this(data);
+        doc.save(cb);
+    },
+    getAll: function (query, cb){
+        this.find(query, cb);
+    },
+    update: function (query, data, cb){
+        this.findOneAndUpdate(query, {$set: data}, {new: true}, cb);
+    },
+    delete: function(query, cb){
+        this.findOneAndDelete(query);
+    }
+};
+
+const dto = mongoose.model("coll_producto", schema);
+module.exports = dto;
